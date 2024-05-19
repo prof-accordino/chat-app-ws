62f8fe7cd4c6ae6a57dd8300ef968150942f12b6
Matteo <clccmatteo@gmail.com>
2024-05-19 18:10:59 +0200

    trying to resolve port problems

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3500;
 const ADMIN = "Admin";
 
 const app = express();
